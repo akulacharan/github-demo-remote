bcac9edab40f3a4e501cd882ff0d76d6400acea7
Charan <61114770+akulacharan@users.noreply.github.com>
2021-12-13 20:13:23 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#Add Implementation
+def Add(x,y):
+    return x+y
+
+#subtract Implementation
+def subtract(x,y):
+    return x-y
+
+#multiply Implementation
+def Multiply(x,y):
+    return x*y
+
+#division Implementation
+def Division():
+    pass
+    
+
+Alekhya
+
+#square implementation
+def square(x):
+    pass
